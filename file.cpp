6c41d47f0d823dc0751dc2e991dd1ce57174ba56
1105-Sullivan-Bradley <bradleysullivan@nevada.unr.edu>
2021-11-05 11:48:40 -0700

    Added comments detailing the loadWavData() method in file.cpp

--- file.cpp
+++ file.cpp
@@ -12,19 +12,41 @@ int WavFile::loadWavData(std::string filename) {
     uint8_t chunkID[4];
     uint32_t chunkSize;
     std::cout << "\nLoading file data...";
+
+    //creates an ifstream object for reading in binary data from 'filename'
     std::ifstream wavFile (filename, std::ios::binary | std::ios::in);
+
+    //checks that the file is opened correctly
     if (wavFile.is_open()) {
+        //Reads in the first 36 bytes of the wav file header
+        //The first 36 bytes of a wav header include the RIFF header and the 'fmt ' header+chunk
         wavFile.read((char*)&wavHeader, sizeof(wavHeader) - 8);
+        /* 
+            This loop reads in the headers of other chunks that might immediately follow the 'fmt '
+            chunk. This could include other metadata that relates to the audio file (artist, year, genre, etc.).
+            All we're interested in is the 'data' chunk. This loop will read in each chunk header ID and chunk
+            size into a buffer, if the chunkID buffer doesn't match the 'data' ID, it will adjust the file pointer
+            forward the size of the chunk which is given in the chunk header until it reads in a 'data' chunk ID.
+        */
         do {
             wavFile.read((char*)chunkID, sizeof(chunkID));
             wavFile.read((char*)&chunkSize, sizeof(chunkSize));
             if (chunkID[0] != 'd' && chunkID[1] != 'a' && chunkID[2] != 't' && chunkID[3] != 'a') wavFile.seekg(chunkSize + wavFile.tellg());
         } while (chunkID[0] != 'd' && chunkID[1] != 'a' && chunkID[2] != 't' && chunkID[3] != 'a');
+
+        //Copies the contents of the chunkID buffer into the wavHeader struct
         for (int i = 0; i < 4; i++) wavHeader.dataID[i] = chunkID[i];
         wavHeader.dataBodySize = chunkSize;
+
+        //Points the sampleData pointer to an array allocated on the heap as we don't want to overflow the stack if the wav file is too large
         sampleData = new int16_t[wavHeader.dataBodySize];
+
+        //Reads in the sample data into the buffer
         wavFile.read((char*)sampleData, wavHeader.dataBodySize);
         wavFile.close();
+
+        //Simple file format checks. Could possibly adapt current implementation to utilize exceptions and more "correct" error
+        //handling methods.
         if (wavHeader.riffID[0] != 'R' && wavHeader.riffID[1] != 'I' && wavHeader.riffID[2] != 'F' && wavHeader.riffID[3] != 'F') {
             std::cout << "File incorrectly formatted. Program requires RIFF .wav files." << std::endl;
             return -1;
