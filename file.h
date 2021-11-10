d6cf6aacb2a4d6f9c80f06a2cd675b67b3fefa4c
1105-Sullivan-Bradley <bradleysullivan@nevada.unr.edu>
2021-11-10 12:51:00 -0800

    Added header comments and prototype documentation (Doxygen format)

--- file.h
+++ file.h
@@ -1,3 +1,9 @@
+/**
+ * @file file.h
+ * @authors Bradley Sullivan, Emily Cuellar, Ever McNeill
+ * @brief Header file for the WavFile class which handles the loading and saving of .wav file sample data.
+ * @date 2021-11-10
+ */
 #ifndef FILE_H
 #define FILE_H
 
@@ -31,12 +37,38 @@ class WavFile {
     int16_t* sampleData;
 
     public:
+        /**
+         * @brief Returns a copy of the sampleData pointer
+         * 
+         * @return int16_t*
+         */
         int16_t* getSampleData() const;
+        /**
+         * @brief Returns a copy of the WAV_HEADER struct
+         * 
+         * @return WAV_HEADER 
+         */
         WAV_HEADER getWavHeader() const;
+        /**
+         * @brief Calculates and returns the duration of the audio file
+         * 
+         * @return double 
+         */
         double getAudioDuration() const;
 
+        /**
+         * @brief Loads the file data into an int16_t buffer
+         * 
+         * @param filename - Name of the .wav file
+         * @return int - Returns 0 if the file is correctly formatted and loaded. Returns -1 if not.
+         */
         int loadWavData(std::string filename);
-        void saveFile(std::string);
+        /**
+         * @brief Saves modified sample data into an existing file. If an existing file doesn't exist, it creates one.
+         * 
+         * @param outputFilename - Name of the file to be written to disk
+         */
+        void saveFile(std::string outputFilename);
 };
 
 #endif //FILE_H
